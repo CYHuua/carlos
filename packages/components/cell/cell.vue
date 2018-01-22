6dd6943261088b33f309de4ec568176a1aac6e82
CCC1993 <940702939@qq.com>
2018-01-22 15:10:49 +0800

    提交txt

---
+++ packages/components/cell/cell.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p class="carlos__cell-title" v-if="title">{{title}}</p>
+    <div class="carlos__cell-slot" :style="styleObj">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'carlos-cell',
+  props: {
+    title: {
+      type: String
+    },
+    gutter: {
+      type: Number,
+      default: 16
+    },
+    bgColor: {
+      type: String,
+      default: '#fff'
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {
+    styleObj () {
+      let styleObj = {}
+      styleObj['marginBottom'] = this.gutter + 'px'
+      styleObj['backgroundColor'] = this.bgColor
+      return styleObj
+    }
+  }
+}
+</script>
---
+++ packages/plugins/message/message.vue
@@ -0,0 +1,44 @@
+<template>
+  <transition name="opacity">
+    <div id="carlos__message" class="carlos__message" v-if="show" :style="{zIndex:zindex}">
+      <div class="message__mask" v-if="mask"></div>
+      <p class="message__content" :class="{'content__long': type === 'long'}">{{content}}</p>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        zindex: 1000,
+        show: false,
+        type: 'short',
+        content: '',
+        mask: false,
+        delay: 2000
+      }
+    },
+    computed: {},
+    mounted () {
+      this.success()
+    },
+    methods: {
+      success () {
+        setTimeout(() => {
+          this.show = false
+        }, this.delay)
+      }
+    }
+  }
+</script>
+
+<style type="text/css">
+  .message__mask{position:fixed;left:0;top:0;z-index:999;width:100%;height:100%;}
+  .message__content{background:rgba(0,0,0,.7);position:fixed;left:50%;top:50%;z-index:1000;-webkit-transform:translate(-50%, -50%);font-size:15px;color:#fff;padding:15px;border-radius:9px;width:auto;text-align:center;}
+  .message__content.content__long{width:80%;}
+  .opacity-enter{opacity:0;}
+  .opacity-enter-active{transition:opacity .4s;}
+  .opacity-leave-active{transition:opacity .2s;}
+  .opacity-leave-active{opacity:0;}
+</style>
